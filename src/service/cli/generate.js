e47dab789220adb324d3a7d8958d6c48fc43467c
Anton Churkin <afrg@yandex.ru>
2022-05-26 21:47:27 +0500

    добавлен CLI. WIP

---
+++ src/service/cli/generate.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const fs = require(`fs`);
+const {ExitCode} = require(`../../constants`);
+const {
+  getRandomInt,
+  shuffle,
+} = require(`../../utils`);
+
+const DEFAULT_COUNT = 1;
+const MAX_COUNT = 1000;
+const FILE_NAME = `mocks.json`;
+
+const TITLES = [
+  `Ёлки. История деревьев`,
+  `Как перестать беспокоиться и начать жить`,
+  `Как достигнуть успеха не вставая с кресла`,
+  `Обзор новейшего смартфона`,
+  `Лучшие рок-музыканты 20-века`,
+  `Как начать программировать`,
+  `Учим HTML и CSS`,
+  `Что такое золотое сечение`,
+  `Как собрать камни бесконечности`,
+  `Борьба с прокрастинацией`,
+  `Рок — это протест`,
+  `Самый лучший музыкальный альбом этого года`,
+];
+
+const ANNOUNCES = [
+  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
+  `Первая большая ёлка была установлена только в 1938 году.`,
+  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
+  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
+  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
+  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
+  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
+  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
+  `Программировать не настолько сложно, как об этом говорят.`,
+  `Простые ежедневные упражнения помогут достичь успеха.`,
+  `Это один из лучших рок-музыкантов.`,
+  `Он написал больше 30 хитов.`,
+  `Из под его пера вышло 8 платиновых альбомов.`,
+  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
+  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
+  `Достичь успеха помогут ежедневные повторения.`,
+  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
+  `Как начать действовать? Для начала просто соберитесь.`,
+  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
+  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
+];
+
+const CATEGORIES = [
+  `Деревья`,
+  `За жизнь`,
+  `Без рамки`,
+  `Разное`,
+  `IT`,
+  `Музыка`,
+  `Кино`,
+  `Программирование`,
+  `Железо`,
+];
+
+const OfferType = {
+  offer: `offer`,
+  sale: `sale`,
+};
+
+const SumRestrict = {
+  min: 1000,
+  max: 100000,
+};
+
+const PictureRestrict = {
+  min: 1,
+  max: 16,
+};
+
+const getPictureFileName = (number) => number > 10 ? `item${number}.jpg` : `item0${number}.jpg`;
+
+const generateOffers = (count) => (
+  Array(count).fill({}).map(() => ({
+    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+    description: shuffle(SENTENCES).slice(1, 5).join(` `),
+    picture: getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max)),
+    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
+    sum: getRandomInt(SumRestrict.min, SumRestrict.max),
+  }))
+);
+
+module.exports = {
+  name: `--generate`,
+  run(args) {
+    const [count] = args;
+    const countArticle = Number.parseInt(count, 10) || DEFAULT_COUNT;
+
+    if (countArticle > MAX_COUNT) {
+      console.error(`Не больше 1000 публикаций`);
+      process.exit(ExitCode.error);
+    }
+
+    const content = JSON.stringify(generateOffers(countArticle));
+
+    fs.writeFile(FILE_NAME, content, (err) => {
+      if (err) {
+        return console.error(`Can't write data to file...`);
+      }
+
+      return console.log(`Operation success. File created.`);
+    });
+
+  }
+};
+
