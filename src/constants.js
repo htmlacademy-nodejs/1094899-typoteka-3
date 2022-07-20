bd679c868e6c2195a15dff24071c3977383ca359
Anton Churkin <afrg@yandex.ru>
2022-07-20 21:06:33 +0500

    запуск сервера CLI

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,12 @@
 
 module.exports.DEFAULT_COMMAND = `--help`;
 module.exports.USER_ARGV_INDEX = 2;
+module.exports.HTTP_CODE = {
+  ok: 200,
+  clientError: 400,
+  notFound: 404,
+  serverError: 500
+};
 module.exports.ExitCode = {
   error: 1,
   success: 0,
