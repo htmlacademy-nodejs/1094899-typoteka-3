988692efcf251a753602827eb5b5105f072862cf
Anton Churkin <afrg@yandex.ru>
2022-08-18 22:31:45 +0500

    article route правка. WIP

--- src/express/routes/article-routes.js
+++ src/express/routes/article-routes.js
@@ -2,10 +2,23 @@
 
 const {Router} = require(`express`);
 const articleRouter = new Router();
+const api = require(`../api`).getAPI();
 
 articleRouter.get(`/:id`, (_req, res) => res.render(`post-detail`));
-articleRouter.get(`/edit/:id`, (_req, res) => res.render(`post`));
-articleRouter.get(`/add`, (_req, res) => res.render(`post`));
+articleRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [article, categories] = await Promise.all([
+    api.getArticle(id),
+    api.getCategories()
+  ]);
+  res.render(`post`, {article, categories});
+});
+articleRouter.get(`/add`, async (_req, res) => {
+  const categories = await api.getCategories();
+
+  res.render(`post`, {categories});
+});
+
 articleRouter.get(`/category/:id`, (_req, res) => res.render(`articles-by-category`));
 
 module.exports = articleRouter;
