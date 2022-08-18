8fd74603619e6fae8b90a0ed823c06c60a981634
Anton Churkin <afrg@yandex.ru>
2022-08-18 15:00:18 +0500

    рефакторинг utils

--- src/constants.js
+++ src/constants.js
@@ -31,3 +31,9 @@ module.exports.Env = {
   DEVELOPMENT: `development`,
   PRODUCTION: `production`
 };
+
+module.exports.DATE_PATTERN = {
+  default: `YYYY-MM-DD HH:mm:ss`,
+  humanReadable: `DD.MM.YYYY, HH:mm`,
+  robotReadable: `YYYY-MM-DDTHH:mm`,
+};
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const dayjs = require(`dayjs`);
-
 module.exports.getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -15,30 +13,4 @@ module.exports.shuffle = (someArray) => {
   }
 
   return someArray;
-};
-
-module.exports.pickRandomDate = (dayOffset) => {
-  if (dayOffset === 0) {
-    throw new Error(`Сдвиг в днях не может быть нулевым`);
-  }
-
-  const randomDayOffset = dayOffset > 0
-    ? exports.getRandomInt(0, dayOffset)
-    : exports.getRandomInt(dayOffset, 0);
-
-  const currentDate = new Date();
-  currentDate.setDate(currentDate.getDate() + randomDayOffset);
-  currentDate.setHours(exports.getRandomInt(0, 23));
-  currentDate.setMinutes(exports.getRandomInt(0, 59));
-  currentDate.setSeconds(exports.getRandomInt(0, 59));
-
-  return new Date(currentDate);
-};
-
-module.exports.humanizeDate = (date) => {
-  if (!(date instanceof Date)) {
-    throw new Error(`${date} не является объектом даты`);
-  }
-
-  return dayjs(date).format(`YYYY-MM-DD HH:mm:ss`);
 };
