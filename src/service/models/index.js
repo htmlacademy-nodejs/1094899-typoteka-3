c56caf907319819d022aec8873598c4bcf142333
Anton Churkin <afrg@yandex.ru>
2023-01-26 13:56:33 +0500

    добавит модель пользователя

--- src/service/models/index.js
+++ src/service/models/index.js
@@ -4,6 +4,7 @@ const defineCategory = require(`./category`);
 const defineComment = require(`./comment`);
 const defineArticle = require(`./article`);
 const defineArticleCategory = require(`./article-category`);
+const defineUser = require(`./user`);
 const Aliase = require(`./aliase`);
 
 const define = (sequelize) => {
@@ -11,6 +12,7 @@ const define = (sequelize) => {
   const Comment = defineComment(sequelize);
   const Article = defineArticle(sequelize);
   const ArticleCategory = defineArticleCategory(sequelize);
+  const User = defineUser(sequelize);
 
   Article.hasMany(Comment, {as: Aliase.COMMENTS, foreignKey: `articleId`, onDelete: `cascade`});
   Comment.belongsTo(Article, {foreignKey: `articleId`});
@@ -18,6 +20,12 @@ const define = (sequelize) => {
   Article.belongsToMany(Category, {through: ArticleCategory, as: Aliase.CATEGORIES});
   Category.belongsToMany(Article, {through: ArticleCategory, as: Aliase.ARTICLES});
   Category.hasMany(ArticleCategory, {as: Aliase.ARTICLE_CATEGORIES});
+
+  User.hasMany(Article, {as: Aliase.ARTICLES, foreignKey: `userId`});
+  Article.belongsTo(User, {as: Aliase.USERS, foreignKey: `userId`});
+
+  User.hasMany(Comment, {as: Aliase.COMMENTS, foreignKey: `userId`});
+  Comment.belongsTo(User, {as: Aliase.USERS, foreignKey: `userId`});
 
   return {Category, Comment, Article, ArticleCategory};
 };
