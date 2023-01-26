e6e9b5fb1c450c465f16e4a402a3d07859ddd11c
Anton Churkin <afrg@yandex.ru>
2023-01-26 15:46:50 +0500

    валидация пользователя и пароля

---
+++ src/service/middlewares/user-validator.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Joi = require(`joi`);
+const {ErrorRegisterMessage, HTTP_CODE} = require(`../../constants`);
+
+const schema = Joi.object({
+  name: Joi.string().pattern(/[^0-9$&+,:;=?@#|'<>.^*()%!]+$/).required().messages({
+    'string.pattern.base': ErrorRegisterMessage.NAME
+  }),
+  email: Joi.string().email().required().messages({
+    'string.email': ErrorRegisterMessage.EMAIL
+  }),
+  password: Joi.string().min(6).required().messages({
+    'string.min': ErrorRegisterMessage.PASSWORD
+  }),
+  passwordRepeated: Joi.string().required().valid(Joi.ref(`password`)).required().messages({
+    'any.only': ErrorRegisterMessage.PASSWORD_REPEATED
+  }),
+  avatar: Joi.string().required().messages({
+    'string.empty': ErrorRegisterMessage.AVATAR
+  })
+});
+
+module.exports = (service) => async (req, res, next) => {
+  const newUser = req.body;
+  const {error} = schema.validate(newUser, {abortEarly: false});
+
+  if (error) {
+    return res.status(HTTP_CODE.clientError)
+      .send(error.details.map((err) => err.message).join(`\n`));
+  }
+
+  const userByEmail = await service.findByEmail(req.body.email);
+
+  if (userByEmail) {
+    return res.status(HTTP_CODE.clientError)
+      .send(ErrorRegisterMessage.EMAIL_EXIST);
+  }
+
+  return next();
+};
+
